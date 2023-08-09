b256e9aa3b135fd0ce8789c4aa40b8c4a82e26af
Slava Kukuyev <slava.kukuyev@gmail.com>
2023-08-10 00:04:01 +0300

    fix directions in single elevator

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	"go.uber.org/zap"
@@ -27,7 +30,7 @@ const _directionDown = "down"
 type Destinations struct {
 	up   map[int][]int
 	down map[int][]int
-	mu   sync.RWMutex
+	//mu   sync.RWMutex
 }
 
 func NewDestinations() *Destinations {
@@ -38,8 +41,8 @@ func NewDestinations() *Destinations {
 }
 
 func (d *Destinations) Append(direction string, fromFloor, toFloor int) {
-	d.mu.Lock()
-	defer d.mu.Unlock()
+	// d.mu.Lock()
+	// defer d.mu.Unlock()
 
 	if direction == _directionUp {
 		d.up[fromFloor] = append(d.up[fromFloor], toFloor)
@@ -51,9 +54,20 @@ func (d *Destinations) Append(direction string, fromFloor, toFloor int) {
 	}
 }
 
+//creates new keys in the same direction
+//removes the request from current floor
+/** example:
+step 1 : map[1] = [3,5] // from the 1st floor requested floors 3 and 5
+step 2 : map[3] = []; map[5] = []; delete map[1] // Elevalor arrived to 1st floor, requested for himself 3,5 floors, and removed 1st floor from the direction slice
+step 3: delete map[3] // elevator arrived to 3d floor
+step 4: delete map[5] // elevator arrived to 5th floor
+
+the same steps in the opposite direction
+*/
+
 func (d *Destinations) Flush(direction string, fromFloor int) {
-	d.mu.Lock()
-	defer d.mu.Unlock()
+	// d.mu.Lock()
+	// defer d.mu.Unlock()
 
 	if direction == _directionUp {
 		if len(d.up[fromFloor]) > 0 {
@@ -68,19 +82,32 @@ func (d *Destinations) Flush(direction string, fromFloor int) {
 		delete(d.up, fromFloor)
 	}
 
+	if direction == _directionDown {
+		if len(d.down[fromFloor]) > 0 {
+			for _, floor := range d.down[fromFloor] {
+				if _, exists := d.down[floor]; !exists {
+					d.down[floor] = make([]int, 0)
+				}
+			}
+
+		}
+
+		delete(d.down, fromFloor)
+	}
+
 }
 
 func (d *Destinations) isUpExisting() bool {
-	d.mu.RLock()
+	//d.mu.RLock()
 	existing := len(d.up) > 0
-	d.mu.RUnlock()
+	//d.mu.RUnlock()
 	return existing
 }
 
 func (d *Destinations) isDownExisting() bool {
-	d.mu.RLock()
+	//d.mu.RLock()
 	existing := len(d.down) > 0
-	d.mu.RUnlock()
+	//d.mu.RUnlock()
 	return existing
 }
 
@@ -113,7 +140,9 @@ func NewElevator(name string) *Elevator {
 
 func (e *Elevator) switchOn() {
 	for range e.switchOnChan {
-		e.Run()
+		if e.destinations.isUpExisting() || e.destinations.isDownExisting() {
+			e.Run()
+		}
 	}
 }
 
@@ -122,32 +151,136 @@ func (e *Elevator) Run() {
 	defer e.mu.Unlock()
 
 	fmt.Printf("The elevator %s is on the %d floor\n", e.name, e.currentFloor)
+	time.Sleep(time.Millisecond * 500)
 
 	if e.direction == _directionUp && e.destinations.isUpExisting() {
 		if _, exists := e.destinations.up[e.currentFloor]; exists {
 			e.openDoor()
 			e.destinations.Flush(e.direction, e.currentFloor)
 			e.closeDoor()
-
 		}
 
-		e.currentFloor++
-		e.switchOnChan <- 1
-	}
+		//if elevator arrived to the top
+		if e.currentFloor == e.maxFloor {
+			e.direction = _directionDown
+			return
+		}
 
+		if e.shouldMoveUp() {
+			e.currentFloor++
+			e.switchOnChan <- 1
+		}
+
+		return
+
+	}
+	//direction up && requests are up
 	if e.direction == _directionDown && e.destinations.isDownExisting() {
 		if _, exists := e.destinations.down[e.currentFloor]; exists {
 			e.openDoor()
 			e.destinations.Flush(e.direction, e.currentFloor)
 			e.closeDoor()
-			e.currentFloor++
+		}
+
+		//check if elevator arrived to the bottom
+		if e.currentFloor == e.minFloor {
+			e.direction = _directionUp
+			return
+		}
+
+		if e.shouldMoveDown() {
+			e.currentFloor--
 			e.switchOnChan <- 1
 		}
+
+		return
 	}
+
+	//case of elevator moving down && no more requests to move down BUT there is a request to move up on the smallest floor
+	//smallest floor of the UP direction which is smaller then current floor
+	if e.direction == _directionDown && e.destinations.isUpExisting() {
+		smallest := findSmallestKey(e.destinations.up)
+		if smallest < e.currentFloor {
+			e.currentFloor--
+			e.switchOnChan <- 1
+			return
+		}
+
+		if smallest == e.currentFloor {
+			e.direction = _directionUp
+			e.switchOnChan <- 1
+			return
+		}
+	}
+
+	//the edge case when elevator moving up && there is no more requests to move up BUT new requests are existing to movedown from the largest floor
+	//largest floor of the DOWN direction which is greater then current floor
+	if e.direction == _directionUp && e.destinations.isDownExisting() {
+		largest := findLargestKey(e.destinations.down)
+		if largest > e.currentFloor {
+			e.currentFloor++
+			e.switchOnChan <- 1
+			return
+		}
+
+		if largest == e.currentFloor {
+			e.direction = _directionDown
+			e.switchOnChan <- 1
+			return
+		}
+	}
+
+}
+
+// check if elevator has more requests in the up direction and should continue move up
+func (e *Elevator) shouldMoveUp() bool {
+	if e.destinations.isUpExisting() {
+		largest := findLargestKey(e.destinations.up)
+		return largest > e.currentFloor
+	}
+
+	return false
+}
+
+// check if elevator has more requests in the down direction and should continue move down
+func (e *Elevator) shouldMoveDown() bool {
+	if e.destinations.isDownExisting() {
+		smallest := findSmallestKey(e.destinations.down)
+		return smallest < e.currentFloor
+	}
+
+	return false
+}
+
+func findLargestKey(m map[int][]int) int {
+	largest := 0
+
+	for key := range m {
+		if key > largest {
+			largest = key
+		}
+	}
+
+	return largest
+}
+
+func findSmallestKey(m map[int][]int) int {
+	smallest := 0
+	first := true
+
+	for key := range m {
+		if first || key < smallest {
+			smallest = key
+			first = false
+		}
+	}
+
+	return smallest
 }
 
 func (e *Elevator) openDoor() {
 	fmt.Printf("Elevator %s opened the doors at floor %d\n", e.name, e.currentFloor)
+	time.Sleep(time.Second * 2)
 }
 
 func (e *Elevator) closeDoor() {
@@ -231,6 +364,28 @@ func (m *Manager) RequestElevator(fromFloor, toFloor int) error {
 
 func main() {
 
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start your main logic in a goroutine
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-signals:
+				fmt.Println("Received termination signal.")
+				return // Exit the loop when a termination signal is received
+			default:
+				// Your main logic goes here
+				//fmt.Println("Working...")
+				time.Sleep(time.Second * 5)
+			}
+		}
+	}()
+
 	initLogger()
 
 	manager := NewManager()
@@ -243,15 +398,30 @@ func main() {
 
 	// Request an elevator going from floor 1 to floor 9
 	if err := manager.RequestElevator(1, 9); err != nil {
-		fmt.Println(err)
+		logger.Error("request elevator 1,9 error", zap.Error(err))
 	}
 
 	// Request an elevator going from floor 3 to floor 5
 	if err := manager.RequestElevator(3, 5); err != nil {
-		fmt.Println(err)
+		logger.Error("request elevator 3,5 error", zap.Error(err))
 	}
 
-	for {
-		time.Sleep(time.Second)
+	// Request an elevator going from floor 3 to floor 5
+	if err := manager.RequestElevator(6, 4); err != nil {
+		logger.Error("request elevator 6,4 error", zap.Error(err))
 	}
+
+	time.Sleep(time.Second * 10)
+
+	if err := manager.RequestElevator(1, 2); err != nil {
+		logger.Error("request elevator 1,2 error", zap.Error(err))
+	}
+
+	time.Sleep(time.Second * 15)
+
+	if err := manager.RequestElevator(7, 0); err != nil {
+		logger.Error("request elevator 7,0 error", zap.Error(err))
+	}
+
+	wg.Wait() // Wait until the main logic is done
 }
