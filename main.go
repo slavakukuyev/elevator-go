0b0b2478cccf818180b9e5f91796a1f4753706b2
Slava Kukuyev <slava.kukuyev@gmail.com>
2023-08-20 22:36:07 +0300

    http server

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
-	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -39,28 +37,6 @@ const _directionUp = "up"
 const _directionDown = "down"
 
 func main() {
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-
-	// Start your main logic in a goroutine
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-signals:
-				logger.Info("received termination signal.")
-				time.Sleep(5 * time.Second)
-				return // Exit the loop when a termination signal is received
-			default:
-				time.Sleep(time.Second * 5)
-			}
-		}
-	}()
-
 	initLogger()
 
 	manager := NewManager()
@@ -71,40 +47,15 @@ func main() {
 	manager.AddElevator(elevator1)
 	manager.AddElevator(elevator2)
 
-	// Request an elevator going from floor 1 to floor 9
-	if err := manager.RequestElevator(1, 9); err != nil {
-		logger.Error("request elevator 1,9 error", zap.Error(err))
-	}
+	port := 8080
+	server := NewServer(port, manager)
 
-	// Request an elevator going from floor 3 to floor 5
-	if err := manager.RequestElevator(3, 5); err != nil {
-		logger.Error("request elevator 3,5 error", zap.Error(err))
-	}
+	// Start the server in a separate goroutine
+	go server.Start()
 
-	// Request an elevator going from floor 3 to floor 5
-	if err := manager.RequestElevator(6, 4); err != nil {
-		logger.Error("request elevator 6,4 error", zap.Error(err))
-	}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
-	time.Sleep(time.Second * 7)
-
-	if err := manager.RequestElevator(1, 2); err != nil {
-		logger.Error("request elevator 1,2 error", zap.Error(err))
-	}
-
-	if err := manager.RequestElevator(7, 9); err != nil {
-		logger.Error("request elevator 1,2 error", zap.Error(err))
-	}
-
-	time.Sleep(time.Second * 10)
-
-	if err := manager.RequestElevator(7, 0); err != nil {
-		logger.Error("request elevator 7,0 error", zap.Error(err))
-	}
-
-	if err := manager.RequestElevator(3, 0); err != nil {
-		logger.Error("request elevator 7,0 error", zap.Error(err))
-	}
-
-	wg.Wait() // Wait until the termination
+	<-stop
+	server.Shutdown()
 }
